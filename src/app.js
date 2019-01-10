43c4d89b0d81489b7ac0c5f03b0b6dd65155b484
beatrizf13 <azz.felix7@gmail.com>
2019-01-10 09:28:50 -0300

    deploy

--- src/app.js
+++ src/app.js
@@ -17,6 +17,14 @@ app.use('/', (req, res, next) => {
 
 require('./app/controllers/index')(app);
 
+//enables CORS
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     next(httpErrors(404));
