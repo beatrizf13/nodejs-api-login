94bd529069ac21f2a5746077905fa74ea221c018
beatrizf13 <azz.felix7@gmail.com>
2019-01-10 09:26:50 -0300

    deploy

--- src/app.js
+++ src/app.js
@@ -8,6 +8,29 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use('/', (req, res, next) => {
+    res.status(200).send({
+        title: "nodejs-api-login",
+        version: "1.0.0"
+    })
+});
+
 require('./app/controllers/index')(app);
 
-app.listen(3000);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(httpErrors(404));
+  });
+  
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+
+module.exports = app;
