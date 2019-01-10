ce230f5f717776e81ed744758d80af9f693427f7
beatrizf13 <azz.felix7@gmail.com>
2019-01-09 23:54:19 -0300

    authenticate whit jsonwebtoken

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://beatrizf13:beatrizf13@ds153304.mlab.com:53304/ndjslogin');
+mongoose.connect('mongodb://beatrizf13:beatrizf13@ds153304.mlab.com:53304/ndjslogin', { useNewUrlParser: true });
+
+mongoose.set('useCreateIndex', true)
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
